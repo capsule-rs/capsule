27d6f097a2ccc660c334430101fb5ddc78082492
drunkirishcoder <daniel.jin@gmail.com>
2019-03-28 11:33:39 -0400

    deparse and cascade

--- framework/src/packets/udp.rs
+++ framework/src/packets/udp.rs
@@ -99,7 +99,7 @@ impl<E: IpPacket> Udp<E> {
     }
 
     #[inline]
-    pub fn set_src_port(&mut self, src_port: u16) {
+    pub fn set_src_port(&self, src_port: u16) {
         self.header().src_port = u16::to_be(src_port);
     }
 
@@ -109,7 +109,7 @@ impl<E: IpPacket> Udp<E> {
     }
 
     #[inline]
-    pub fn set_dst_port(&mut self, dst_port: u16) {
+    pub fn set_dst_port(&self, dst_port: u16) {
         self.header().dst_port = u16::to_be(dst_port);
     }
 
@@ -119,7 +119,7 @@ impl<E: IpPacket> Udp<E> {
     }
 
     #[inline]
-    pub fn set_length(&mut self, length: u16) {
+    pub fn set_length(&self, length: u16) {
         self.header().length = u16::to_be(length);
     }
 
@@ -129,7 +129,7 @@ impl<E: IpPacket> Udp<E> {
     }
 
     #[inline]
-    pub fn set_checksum(&mut self, checksum: u16) {
+    pub fn set_checksum(&self, checksum: u16) {
         self.header().checksum = u16::to_be(checksum);
     }
 
@@ -223,6 +223,17 @@ impl<E: IpPacket> Packet for Udp<E> {
     fn remove(self) -> Result<Self::Envelope> {
         buffer::dealloc(self.mbuf, self.offset, self.header_len())?;
         Ok(self.envelope)
+    }
+
+    #[inline]
+    fn cascade(&self) {
+        self.set_length(self.len() as u16);
+        self.envelope().cascade();
+    }
+
+    #[inline]
+    fn deparse(self) -> Self::Envelope {
+        self.envelope
     }
 }
 
