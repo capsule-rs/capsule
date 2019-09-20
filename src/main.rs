f5a2a6993babfef428cb573415578df29277da79
drunkirishcoder <daniel.jin@gmail.com>
2019-09-20 17:08:47 -0400

    mempool create

--- src/main.rs
+++ src/main.rs
@@ -28,12 +28,14 @@ fn main() -> Result<(), Error> {
         "--log-level=9",
         "-l",
         "0-3",
-        "--proc-type",
-        "primary",
+        "-w",
+        "00:08.0",
         "-v",
     ];
     dpdk::eal_init(&args)?;
     let nb_ports = dpdk::eth_dev_count_avail();
     println!("HOORAY!!! {} ports available.", nb_ports);
+    let mempool = dpdk::Mempool::create("dump", 8191, 250)?;
+    println!("mempool '{}' created.", mempool.name());
     Ok(())
 }
