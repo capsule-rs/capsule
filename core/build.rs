9ba5c96be7769f176c6772b183bad790a9b89e9f
Zeeshan Lakhani <zeeshan_lakhani@comcast.com>
2019-10-28 11:28:17 -0400

    - top-level examples akin to similar workspace/groupings: https://github.com/SergioBenitez/Rocket - clean-up build.rs files to use rte_sdk - remove [[example]] as we rather you build local (todo: update readme)

--- core/build.rs
+++ core/build.rs
@@ -3,8 +3,13 @@
 // will decide to not link functions not explicitly called but
 // must be included in the final binary.
 
+use std::env;
+
 fn main() {
+    let rte_sdk = env::var("RTE_SDK").expect("No RTE_SDK found ~ DPDK installation directory.");
+
+    println!("cargo:rustc-link-search=native={}/build/lib", rte_sdk);
     // need to statically link the mempool ring driver for `cargo test`
-    println!("cargo:rustc-link-search=native=/opt/dpdk/build/lib");
     println!("cargo:rustc-link-lib=static=rte_mempool_ring");
+    println!("cargo:rerun-if-env-changed=RTE_SDK");
 }
