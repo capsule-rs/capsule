f4be0c1201ff15edd9d28ace2f8ad360fd9222ae
drunkirishcoder <daniel.jin@gmail.com>
2019-11-11 14:56:16 -0500

    collect kni interface stats

--- examples/kni/main.rs
+++ examples/kni/main.rs
@@ -1,11 +1,21 @@
+use metrics_core::{Builder, Drain, Observe};
+use metrics_runtime::observers::YamlBuilder;
+use nb2::metrics;
 use nb2::settings::load_config;
 use nb2::{batch, Result, Runtime};
+use std::time::Duration;
 use tracing::{debug, Level};
 use tracing_subscriber::fmt;
 
+fn print_stats() {
+    let mut observer = YamlBuilder::new().build();
+    metrics::global().controller().observe(&mut observer);
+    println!("{}", observer.drain());
+}
+
 fn main() -> Result<()> {
     let subscriber = fmt::Subscriber::builder()
-        .with_max_level(Level::DEBUG)
+        .with_max_level(Level::INFO)
         .finish();
     tracing::subscriber::set_global_default(subscriber)?;
 
@@ -17,5 +27,6 @@ fn main() -> Result<()> {
             batch::splice(q.clone(), q.kni().unwrap().clone())
         })?
         .add_kni_rx_pipeline_to_port("kni0", batch::splice)?
+        .add_periodic_task_to_core(0, print_stats, Duration::from_secs(1))?
         .execute()
 }
