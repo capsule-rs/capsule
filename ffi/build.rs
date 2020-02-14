ac46da2e68638d348ebb0a15bb898cce769d4373
Zeeshan Lakhani <zeeshan_lakhani@comcast.com>
2020-02-14 01:10:15 -0500

    use dpdk and co through dy(namic)libs based on new meson install and paths
    - update build.rs needs accordingly
    - lint/cleanup

--- ffi/build.rs
+++ ffi/build.rs
@@ -21,10 +21,126 @@ use cc;
 use std::env;
 use std::path::{Path, PathBuf};
 
+const RTE_CORE_LIBS: &[&str] = &[
+    "rte_acl",
+    "rte_bbdev",
+    "rte_bitratestats",
+    "rte_bpf",
+    "rte_bus_dpaa",
+    "rte_bus_fslmc",
+    "rte_bus_ifpga",
+    "rte_bus_pci",
+    "rte_bus_vdev",
+    "rte_bus_vmbus",
+    "rte_cfgfile",
+    "rte_cmdline",
+    "rte_common_cpt",
+    "rte_common_dpaax",
+    "rte_common_octeontx",
+    "rte_compressdev",
+    "rte_cryptodev",
+    "rte_distributor",
+    "rte_eal",
+    "rte_efd",
+    "rte_ethdev",
+    "rte_eventdev",
+    "rte_flow_classify",
+    "rte_gro",
+    "rte_gso",
+    "rte_hash",
+    "rte_ip_frag",
+    "rte_jobstats",
+    "rte_kni",
+    "rte_kvargs",
+    "rte_latencystats",
+    "rte_lpm",
+    "rte_mbuf",
+    "rte_member",
+    "rte_mempool",
+    "rte_mempool_bucket",
+    "rte_mempool_dpaa",
+    "rte_mempool_dpaa2",
+    "rte_mempool_octeontx",
+    "rte_mempool_ring",
+    "rte_mempool_stack",
+    "rte_meter",
+    "rte_metrics",
+    "rte_net",
+    "rte_pci",
+    "rte_pdump",
+    "rte_pipeline",
+    "rte_port",
+    "rte_power",
+    "rte_rawdev",
+    "rte_reorder",
+    "rte_ring",
+    "rte_sched",
+    "rte_security",
+    "rte_table",
+    "rte_timer",
+    "rte_vhost",
+];
+
+static RTE_PMD_LIBS: &[&str] = &[
+    "rte_pmd_af_packet",
+    "rte_pmd_ark",
+    "rte_pmd_atlantic",
+    "rte_pmd_avf",
+    "rte_pmd_avp",
+    "rte_pmd_axgbe",
+    "rte_pmd_bbdev_null",
+    "rte_pmd_bnxt",
+    "rte_pmd_bond",
+    "rte_pmd_caam_jr",
+    "rte_pmd_crypto_scheduler",
+    "rte_pmd_cxgbe",
+    "rte_pmd_dpaa",
+    "rte_pmd_dpaa2",
+    "rte_pmd_dpaa2_cmdif",
+    "rte_pmd_dpaa2_event",
+    "rte_pmd_dpaa2_qdma",
+    "rte_pmd_dpaa2_sec",
+    "rte_pmd_dpaa_event",
+    "rte_pmd_dpaa_sec",
+    "rte_pmd_dsw_event",
+    "rte_pmd_e1000",
+    "rte_pmd_ena",
+    "rte_pmd_enetc",
+    "rte_pmd_enic",
+    "rte_pmd_failsafe",
+    "rte_pmd_fm10k",
+    "rte_pmd_i40e",
+    "rte_pmd_ifc",
+    "rte_pmd_ifpga_rawdev",
+    "rte_pmd_ixgbe",
+    "rte_pmd_kni",
+    "rte_pmd_netvsc",
+    "rte_pmd_nfp",
+    "rte_pmd_null",
+    "rte_pmd_null_crypto",
+    "rte_pmd_octeontx",
+    "rte_pmd_octeontx_crypto",
+    "rte_pmd_opdl_event",
+    "rte_pmd_pcap",
+    "rte_pmd_qat",
+    "rte_pmd_qede",
+    "rte_pmd_ring",
+    "rte_pmd_skeleton_event",
+    "rte_pmd_skeleton_rawdev",
+    "rte_pmd_softnic",
+    "rte_pmd_sw_event",
+    "rte_pmd_tap",
+    "rte_pmd_vdev_netvsc",
+    "rte_pmd_vhost",
+    "rte_pmd_virtio",
+    "rte_pmd_virtio_crypto",
+];
+
+const RTE_DEPS_LIBS: &[&str] = &["numa", "pcap"];
+
 fn bind(path: &Path) {
     cc::Build::new()
         .file("src/shim.c")
-        .include("/opt/dpdk/build/include")
         .flag("-march=native")
         .compile("rte_shim");
 
@@ -40,8 +156,8 @@ fn bind(path: &Path) {
         .derive_default(true)
         .derive_partialeq(true)
         .default_enum_style(bindgen::EnumVariation::ModuleConsts)
-        .clang_arg("-fkeep-inline-functions")
-        .clang_arg("-I/opt/dpdk/build/include")
+        .clang_arg("-finline-functions")
+        .clang_arg("-march=native")
         .rustfmt_bindings(true)
         .generate()
         .expect("Unable to generate bindings")
@@ -51,15 +167,22 @@ fn bind(path: &Path) {
 
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let rte_sdk = env::var("RTE_SDK").expect("No RTE_SDK found ~ DPDK installation directory.");
-
-    // there's a problem statically linking to a linker script
-    // see: https://github.com/rust-lang/rust/issues/40483
-    println!("cargo:rustc-link-search=native={}/build/lib", rte_sdk);
-    println!("cargo:rustc-link-lib=dylib=dpdk");
-    println!("cargo:rustc-link-lib=dylib=numa");
-    println!("cargo:rustc-link-lib=dylib=pcap");
-    println!("cargo:rustc-link-lib=dylib=z");
 
     bind(&out_path);
+
+    RTE_CORE_LIBS
+        .iter()
+        .for_each(|lib| println!("cargo:rustc-link-lib=dylib={}", lib));
+
+    RTE_PMD_LIBS
+        .iter()
+        .for_each(|lib| println!("cargo:rustc-link-lib=dylib={}", lib));
+
+    RTE_DEPS_LIBS
+        .iter()
+        .for_each(|lib| println!("cargo:rustc-link-lib=dylib={}", lib));
+
+    // re-run build.rs upon changes
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=src/");
 }
